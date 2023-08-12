7709ff5d56ce92a717e8dfed10c1e249fe93eb3e
anouk <anoukvandewetering@live.nl>
2023-08-12 13:23:44 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,12 @@
+mijn_prijzen = {"aardbei" : 3, "vanille" : 4, "chocolade" : 5}
+aanbieding = mijn_prijzen["vanille"] * 0.8
+reclame_tekst = f"vandaag in de aanbieding: vanille, 1 liter - slechts €{aanbieding}."
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+print(reclame_tekst4)
+for el in reclame_tekst4:
+    if len(el) > 4:
+        print(el.upper())
+    else:
+        print(el.lower())
